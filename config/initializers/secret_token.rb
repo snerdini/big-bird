73e770eabae97f9686f8121e360324d9b2e8364f
steve <snerdini@gmail.com>
2013-05-19 15:33:56 -0400

    Created initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigBird::Application.config.secret_token = 'a91a134ad4dc5d052cb03612c017b9af1a7b24a1d18bacea269e40d8fe9a311707c0d1a05764a4f13c6dacca34b1ef918083eb2388b844b9d7321045935a9ed2'
