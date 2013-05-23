f5de8ab2a596d61502e9c58e97d60dca75989f92
Steve Miller <snerdini@gmail.com>
2013-05-22 23:37:35 -0400

    Finished layouts and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 BigBird::Application.routes.draw do
+  get "users/new"
+
   # Define static page routes
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,6 +66,4 @@ BigBird::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-
-  root :to => 'application#index'
 end
