5826759cd3df34ebfbc5c0bdd492bfc90cf865b6
Steve Miller <snerdini@gmail.com>
2013-06-03 22:54:14 -0400

    Added update method for user profile.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,7 @@
 include ApplicationHelper
 
 def sign_in(user)
-  visit sign_in_path
+  visit signin_path
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
   click_button 'Sign in'
